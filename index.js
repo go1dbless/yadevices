2a2c4fc0c47893a651f8c70979468c8b9de01eb0
Vlad Bondar <strongbondar@gmail.com>
2023-05-03 04:01:38 +0600

    add devices

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
 const http = require("http")
 const PORT = process.env.PORT || 8080
 const server = http.createServer()
+const axios = require('axios');
+const fs = require('fs');
 
-server.on("request", (req, res) => {
+axios.defaults.headers.common['Authorization'] = `Bearer y0_AgAAAAAbar_TAAnZZAAAAADiVd0H3Tr-NAznQXqERqvtS2qUNb7GHAQ`;
+axios.defaults.headers.common['Content-Type'] = `application/json`;
+axios.defaults.baseURL = "https://api.iot.yandex.net/v1.0";
+
+
+const LAMP_ID = "d83de6ad-8e34-4173-8d6b-dfe736a5af8f";
+const SOCKET_ID = "b884d23f-64b9-4449-a803-a8c6a43738d6";
+
+server.on("request", async (req, res) => {
 
     if (req.url === '/') {
         res.write("Home page")
+    }
+
+    if (req.url === '/devices') {
+        const response = await axios.get('/user/info');
+        fs.writeFile('devices.json', JSON.stringify(response.data), err => {
+            if (err) {
+                console.log('Error writing file:', err);
+            } else {
+                console.log('Data saved to file');
+            }
+        });
+    }
+
+    if (req.url === '/socket') {
+        const response = await axios.get('/devices/'+SOCKET_ID);
+        var newState = !response.data.capabilities.filter((e) => e.type === 'devices.capabilities.on_off')[0].state.value
+
+        await axios.post('/devices/actions', {
+            "devices": [
+                {
+                    "id": SOCKET_ID,
+                    "actions": [
+                        {
+                            "type": "devices.capabilities.on_off",
+                            "state": {
+                                "instance": "on",
+                                "value": newState
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+        );
+    }
+
+    if (req.url === '/lamp') {
+        const response = await axios.get('/devices/'+LAMP_ID);
+        var newState = !response.data.capabilities.filter((e) => e.type === 'devices.capabilities.on_off')[0].state.value
+
+        await axios.post('/devices/actions', {
+            "devices": [
+                {
+                    "id": LAMP_ID,
+                    "actions": [
+                        {
+                            "type": "devices.capabilities.on_off",
+                            "state": {
+                                "instance": "on",
+                                "value": newState
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+        );
     }
 
     res.statusCode = 200
